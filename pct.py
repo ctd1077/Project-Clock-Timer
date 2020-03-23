9dd53274fd05c51190fe2c7039b36a7a32ff1bbc
Cam D <41445364+ctd1077@users.noreply.github.com>
2020-03-22 20:27:25 -0600

    fixed countdown added to .py

--- pct.py
+++ pct.py
@@ -1,38 +1,35 @@
 #!/usr/bin/env python3
 # Project Clock Timer
-import time
-import sys
-# from . import countui # import tkinter UI
+#import time
+#import sys
+from datetime import datetime
 
-print('Enter lenght of time for project')
-day=input('Enter # of days: ')
-hour=input('Enter # of hours: ')
+def days_between(d1, d2):
+    d1 = datetime.strptime(d1, "%Y-%m-%d-%H-%M-%S")
+    d2 = datetime.strptime(d2, "%Y-%m-%d-%H-%M-%S")
+    return abs((d2 - d1))
 
-days=int(day)
-hours=int(hour)
-min = 0
-sec= 60
+def days_hours_minutes_sec(td):
+    return td.days, td.seconds//3600, (td.seconds//60)%60, (td.seconds)%60
 
-eop = 'Times Up'
-unc = 'Under Construction'
- 
-while days > -1:
-    while hours > -1:
-        while min > -1:
-            while sec > 0:
-                sec=sec-1
-                time.sleep(1)
-                s = ('%02.f' % sec)  # format
-                m = ('%02.f' % min)
-                h = ('%02.f' % hours)
-                d = ('%02.f' % days)
-                sys.stdout.write('\r' + str(d) + ':' + str(h) + ':' + str(m) + ':' + str(s))
-                print(': '+ unc, end='\r')
-            min=min-1
-            sec=60
-        hours=hours-1
-        min=59
-    days=days-1
-    hours=23
 
-Print(eop)
+
+if __name__ == '__main__':
+    pn=input("What's the name of project? ")
+    year=input('Enter Year the project will be finished: ')
+    day=input('Enter # of days: ')
+    hour=input('Enter # of hours: ')
+    das = '-'
+    # Need to fix the format of the date entered
+    # go back to JN
+    #d1 = year+das+day+das+hour+das+'0'+das+'0'+das+'0'
+    d1 = '2020-06-12-0-0-0'
+    while True:
+        current_date_and_time = datetime.now()
+        a = str(current_date_and_time.date())
+        b = str(current_date_and_time.time())
+        rp = b.replace(":", "-")
+        sp = rp.split('.')[0]
+        d2 = a+'-'+sp
+        td = days_between(d1, d2)
+        print(days_hours_minutes_sec(td), end='\r')
